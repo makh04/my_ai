a88f7c82080f4fd5238bac34650da75f2dc9f756
makh04 <nabilmukerrobmakhtum@gmail.com>
2025-08-10 09:57:23 +0600

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -172,8 +172,7 @@ export default function RootLayout({
         {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/faviconv2.png" />
         <link rel="manifest" href="/site.webmanifest" />
 
         {/* Preconnect to external domains for performance */}
