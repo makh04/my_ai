3e5ad88f7d3783ae502a20fc9a3b6a47401c54d8
makh04 <nabilmukerrobmakhtum@gmail.com>
2025-09-19 11:58:59 +0600

    new download_button update

--- app/updates/data.ts
+++ app/updates/data.ts
@@ -10,6 +10,38 @@ export interface UpdatePost {
 }
 
 export const updatePosts: UpdatePost[] = [
+      {
+    id: "3",
+    title: "Custom Automation according to prompt-Pika AI ",
+    description: "Now you can automate every possible task which can be done by keyboard and According to your wish.",
+    date: "2025-09-19",
+    version: "v1",
+    type: "feature",
+    slug: "full_custom_automation",
+    content: `
+      ## What's New: Make custom automation command in your wish
+
+      Big news! Now you can make your own command Like telling pika to open chrome then type .... then press enter and much more custom commands. It is like you can give details information to Pika AI voice assistant. and there is no limit of how many step you tell to Pika.assistant
+
+      So, basically you can perform all task that is possible to do with keyboard. You can break the process and tell him to do this-this. For example you can say "Pika do this then click this then press this then again to this then repeat this step multiple times". This is cool right ?
+
+      Let me show you some more examples:
+
+      Examples,
+
+      1) Pika open chrome then type the link of instagram's reels page link then press enter then press down button for scrolling and make the time interval for every reels and scroll 60 second and after watching 20 reels turn ths tab off
+      BOOM ! Now pika will let you watch 10 reels then turn the tab off
+
+      It's pretty straightforward, but super useful for multitaskers and people who love to keep things organized. And you can make more custom task like this
+
+      Features:
+      - Automate about all the possible task
+      - Save the same prompt and use it as custom automation
+      - Make your own function which would make it your own personal AI Assistant
+
+      Hope this makes your workflow smoother! As always, let us know if you have any thoughts or suggestions.
+    `,
+  },
   {
     id: "2",
     title: "Run Any App + Session History",
